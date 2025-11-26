3f867bf7375ed38e3bf398bdee16e7d7251a0de5
vanssie <ayano.kikiyo08@gmail.com>
2025-11-26 13:40:01 +0700

    Add files via upload

---
+++ modul1/isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>");
+document.writeln("<HR WIDHT=600 COLOR=BLACK>");
+document.writeln("<H1>SMK AL-GHIFARI BANYURESMI</H1>");
+document.writeln("<H2>KAREUEUS GARUT</H2>");
+document.writeln("<H3>JURUSAN REKAYASA PERANGKAT LUNAK</H3>");
+document.writeln("<HR WIDTH=600 COLOR=BLACK>");
+document.writeln("</CENTER>");
